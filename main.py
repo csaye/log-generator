f3395eb2d5ee74d4eb54f48cb174187e790ef800
Cooper Saye <coopersaye@gmail.com>
2022-01-20 21:53:23 -0800

    Create log file.

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ from datetime import date
 # get log name with current date
 today = date.today()
 filename = f'{today.strftime("%Y-%m-%d")}.log.md'
+
+# create log file
+file = open(f'./logs/{filename}', 'w')
+file.close()
