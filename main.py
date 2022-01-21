89912f13083e0ee303946ac4daa9f737c1132cd7
Cooper Saye <coopersaye@gmail.com>
2022-01-20 23:03:44 -0800

    Catch config errors.

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ try:
     config_json = config_file.read()
     config = json.loads(config_json)
     config_file.close()
+except FileNotFoundError:
+    print('config.json not found')
+    sys.exit()
+except json.decoder.JSONDecodeError:
+    print('invalid config.json')
+    sys.exit()
 
 # fetch github actions
 url = f'https://api.github.com/users/{config["username"]}/events'
