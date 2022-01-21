8036290659fdaad2cff63621d5d47efa5026e7b1
Cooper Saye <coopersaye@gmail.com>
2022-01-20 22:28:14 -0800

    Fetch GitHub actions.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
+import json, requests
 from datetime import date
 
 # read config
@@ -6,6 +6,11 @@ config_file = open('./config.json', 'r')
 config_json = config_file.read()
 config = json.loads(config_json)
 config_file.close()
+
+# fetch github actions
+url = f'https://api.github.com/users/{config["username"]}/events'
+response = requests.get(url)
+actions = response.json()
 
 # get log name with current date
 today = date.today()
