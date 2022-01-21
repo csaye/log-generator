687b16287b6fa6253be7eb68956eb75b9dca7497
Cooper Saye <coopersaye@gmail.com>
2022-01-20 22:01:10 -0800

    Read config file.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import json
 from datetime import date
+
+# read config
+config_file = open('./config.json', 'r')
+config_json = config_file.read()
+config = json.loads(config_json)
+config_file.close()
 
 # get log name with current date
 today = date.today()
