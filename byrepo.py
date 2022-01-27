a85a406ffb5743b26961c8f962a425ddaf583ba4
Cooper Saye <coopersaye@gmail.com>
2022-01-26 16:40:40 -0800

    Get log date and time.

--- byrepo.py
+++ byrepo.py
@@ -18,5 +18,20 @@ if len(sys.argv) < 3:
     print('date and repo arguments must be given')
     sys.exit()
 
+# get date
+log_date = sys.argv[1]
+if log_date == 'today': log_date = date.today()
+elif log_date == 'yesterday': log_date = date.today() - timedelta(days=1)
+else:
+    try:
+        log_date = datetime.strptime(log_date, '%Y-%m-%d').date()
+    except ValueError:
+        print('invalid date given (YYYY-MM-DD)')
+        sys.exit()
+
+# get times
+day_name = log_date.strftime('%A')
+times = config['times'][day_name]
+
 # get repo
 repo = sys.argv[2]
