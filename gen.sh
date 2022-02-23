4757c55839fd5002f923853d11d101552de6311c
Cooper Saye <coopersaye@gmail.com>
2022-02-22 17:15:46 -0800

    Split generator by type.

--- gen.sh
+++ gen.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-python3 ./byrepo.py $1 $2
+if [ $1 == "repo" ]
+then
+  python3 ./byrepo.py $1 $2
+elif [ $1 == "date" ]
+then
+  python3 ./bydate.py $1
+elif [ $1 == "range" ]
+then
+  python3 ./byrange.py
+else
+  echo "invalid command"
+  echo "usage: ./gen.sh <repo | date | range> [date (YYYY-MM-DD)] [repo]"
+fi
