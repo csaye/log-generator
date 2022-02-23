45555facacadecaca7085bcf3e53f4dcbe242d81
Cooper Saye <coopersaye@gmail.com>
2022-02-22 17:16:36 -0800

    Retrieve and verify range dates.

--- byrange.py
+++ byrange.py
@@ -12,3 +12,23 @@ except FileNotFoundError:
 except json.decoder.JSONDecodeError:
     print('invalid config.json')
     sys.exit()
+
+# get start and end dates
+start_date = config['start_date']
+end_date = config['end_date']
+
+# verify start date
+try:
+    start_date = datetime.strptime(start_date, '%Y-%m-%d').date()
+except ValueError:
+    print('invalid start date given (YYYY-MM-DD)')
+    sys.exit()
+
+# verify end date
+try:
+    end_date = datetime.strptime(end_date, '%Y-%m-%d').date()
+except ValueError:
+    print('invalid end date given (YYYY-MM-DD)')
+    sys.exit()
+
+log_date = start_date
